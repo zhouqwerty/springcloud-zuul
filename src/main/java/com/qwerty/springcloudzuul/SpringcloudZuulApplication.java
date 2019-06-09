a7590bad9c1b9c5395cca91eee259499d1f94437
zhou <2891128968@qq.com>
2019-06-09 11:23:58 +0800

    Initial commit

---
+++ src/main/java/com/qwerty/springcloudzuul/SpringcloudZuulApplication.java
@@ -0,0 +1,13 @@
+package com.qwerty.springcloudzuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcloudZuulApplication.class, args);
+	}
+
+}
